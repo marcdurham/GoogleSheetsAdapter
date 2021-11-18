e3dcc15ed85cd1c73c4d492099d3304326d52d1b
Marc Durham <marcdurham@gmail.com>
2021-11-18 13:31:55 -0800

    Create methods to read and write a sheet range

---
+++ GoogleAdapter/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using GoogleAdapter;
+
+Console.WriteLine("Reading and writing to a Google spreadsheet...");
+string secretsJsonPath = args[0];
+string documentId = args[1];
+string range = args[2];
+
+var tester = new Sheets(secretsJsonPath);
+
+IList<IList<object>> linesToEdit = tester.Read(documentId: documentId, range: range);
+
+//for (int r = 4; r < 7; r++)
+//{
+//    linesToEdit[r][2] = "2/3/1111";
+//}
+
+var oValues = new[] { new[] { (object)"2/4/1111" } };
+
+tester.Write(
+    documentId: documentId,
+    range: range,
+    values: oValues); // new[] { new[] { (object)"2/5/1111" } });
+
+IList<IList<object>> lines = tester.Read(documentId: documentId, range: range);
+
+foreach(IList<object> line in lines)
+{
+    foreach(object value in line)
+    {
+        Console.Write($"{value}, ");
+    }
+    Console.WriteLine();
+}
---
+++ GoogleAdapter/Sheets.cs
@@ -0,0 +1,86 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+
+namespace GoogleAdapter
+{
+    internal class Sheets
+    {
+        // Some APIs, like Storage, accept a credential in their Create()
+        // method.
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly, SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Google Sheets API .NET Experiment";
+        const string CredPath = "token.json";
+
+        readonly SheetsService _service;
+
+        public Sheets(string jsonPath)
+        {
+            _service = GetSheetService(jsonPath);
+        }
+
+        public void WriteOneCell(string documentId, string range, object value)
+        {
+            Write(documentId, range, new[] { new[] { value } });
+        }
+
+        public void Write(string documentId, string range, IList<IList<object>> values)
+        {
+            ValueRange valueRange = new() { Values = values };
+            SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest =
+                _service.Spreadsheets.Values.Update(valueRange, documentId, range);
+
+            updateRequest.ValueInputOption =
+                SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+
+            updateRequest.Execute();
+        }
+
+        public IList<IList<object>> Read(string documentId, string range)
+        {
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                _service.Spreadsheets.Values.Get(documentId, range);
+
+            ValueRange response = request.Execute();
+
+            return response.Values;
+        }
+
+        static SheetsService GetSheetService(string jsonPath)
+        {
+            UserCredential credential = Credentials(jsonPath);
+
+            // Create Google Sheets API service.
+            SheetsService service = new(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
+        }
+
+        static UserCredential Credentials(string jsonPath)
+        {
+            UserCredential credential;
+
+            using (FileStream stream = new(jsonPath, FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(CredPath, true)).Result;
+                
+                Console.WriteLine("Credential file saved to: " + CredPath);
+            }
+
+            return credential;
+        }
+    }
+}

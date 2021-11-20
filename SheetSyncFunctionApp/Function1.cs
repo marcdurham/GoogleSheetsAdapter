94194a45114751a9dccfb60177ba3c667aeddfd8
Marc Durham <marcdurham@gmail.com>
2021-11-20 01:45:44 -0800

    Add SengGrid email

--- SheetSyncFunctionApp/Function1.cs
+++ SheetSyncFunctionApp/Function1.cs
@@ -1,9 +1,14 @@
 using GoogleAdapter.Adapters;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SheetSyncFunctionApp
 {
@@ -22,7 +27,45 @@ namespace SheetSyncFunctionApp
             string range = Environment.GetEnvironmentVariable("Range", EnvironmentVariableTarget.Process);
             string targetDocumentId = Environment.GetEnvironmentVariable("TargetDocumentId", EnvironmentVariableTarget.Process);
             string targetRange = Environment.GetEnvironmentVariable("TargetRange", EnvironmentVariableTarget.Process);
+            string sendGridApiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY", EnvironmentVariableTarget.Process);
 
+            CopySheet(log, secretsJson, documentId, range, targetDocumentId, targetRange);
+
+            var sheets = new Sheets(secretsJson, isServiceAccount: true);
+
+            IList<IList<object>> rows = sheets.Read(documentId: documentId, range: range);
+
+            foreach (IList<object> row in rows)
+            {
+                string name = $"{row[0]}";
+                string email = $"{row[1]}";
+                log.LogInformation($"Sending email to {name}: {email} ...");
+                Response response = SendEmail(name, email, sendGridApiKey).Result;
+                log.LogInformation($"Status Code:{response.StatusCode}");
+            }
+        }
+
+        private static async Task<Response> SendEmail(string name, string email, string sendGridApiKey)
+        {
+            //HttpClient client = new();
+            //HttpRequestMessage request = new(HttpMethod.Get, "https://api.sendgrid.com/v3/mail/send");
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sendGridApiKey);
+            //client.DefaultRequestHeaders.
+            //client.SendAsync(request);
+            ////var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(sendGridApiKey);
+            var from = new EmailAddress("auto@territorytools.org", "Territory Tools System");
+            var subject = "Test Email";
+            var to = new EmailAddress(email, name);
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+            return response;
+        }
+
+        private static void CopySheet(ILogger log, string secretsJson, string documentId, string range, string targetDocumentId, string targetRange)
+        {
             log.LogInformation($"secretsJsonPath:{secretsJson.Length} documentId:{documentId} range:{range}");
 
             var sheets = new Sheets(secretsJson, isServiceAccount: true);
@@ -35,19 +78,6 @@ namespace SheetSyncFunctionApp
                 documentId: targetDocumentId,
                 range: targetRange,
                 values: values);
-
-            //IList<IList<object>> lines = tester.Read(documentId: documentId, range: range);
-
-            //foreach (IList<object> line in lines)
-            //{
-            //    var builder = new StringBuilder(3000);
-            //    foreach (object value in line)
-            //    {
-            //        builder.Append($"{value}, ");
-            //    }
-
-            //    log.LogInformation(builder.ToString());
-            //}
         }
     }
 }
